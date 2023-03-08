cee84b04bc3ef7f50259f819b5efee9464cf94e5
TRextabat <117582815+TRextabat@users.noreply.github.com>
2023-03-08 12:35:43 +0300

    Add files via upload

--- firstcode.py
+++ firstcode.py
@@ -2,29 +2,32 @@ import numpy as np
 import json
 THERESHOLD = 5 #TODO ask value
 WINDOW = 3 #TODO ask value
+PEAK_CHANGE = 100
 def main():
-    preopen = open("/home/user/D/BAMdata/proje/bedfilem.bed" ,"r") # This is just for count length to runout from unwanted calculates in program
-    preseq = preopen.readline().split()
-    preopen.close()
-    length = int(preseq[2]) - int(preseq[1]) # as I recognize seq lengths are same every where
     bedfile = (row for row in open("/home/user/D/BAMdata/proje/bedfilem.bed" ,"r")) #TODO I will use sampy 
-    output = open("output.txt","w")
+    output = open("output.bed","w")
     ittirate = 0
     data_dic = {}#TODO will change to array
-    chr, start, end = 0, 0, 0
+    chr, start, end = 0, 0,0 #would use in future
     signal = False
     peak_start_pos, peak_end_pos = 0, 0
+    last_peak_end, new_peak_start = 0, 0
+    print("in progress...")
     while True:        
-        seq = next(bedfile).split()# TODO it will be a function
-        chr1, start1, end1 = seq[0], int(seq[1]), int(seq[1])+length
-        print(seq)
-        for nucleotid in range(start1, end1+1):
+        try:
+            seq = next(bedfile).split()# TODO it will be a function
+        except StopIteration:
+            break
+        chr1, start1, end1 = seq[0], int(seq[1]), int(seq[2])
+        if chr1 != chr:
+            data_dic.clear()
+            signal = False
+        
+        for nucleotid in range(start1, end1+1):#TODO will be array
             try:
                 data_dic[nucleotid] += 1
             except KeyError:
                 data_dic[nucleotid] = 1
-        
-        
         
         if signal == False:
             peak_start_pos, signal = peak_start(start1, data_dic)
@@ -33,15 +36,13 @@ def main():
             if signal == False:
                 peak_bed = f"{chr}\t{peak_start_pos}\t{peak_end_pos}\n"
                 output.write(peak_bed)
-        start, end = start1, end1
+        
         if ittirate != 0:
             cleaning(start, start1, data_dic)
-
-
+        chr,start, end = chr1,start1, end1
         ittirate += 1
-
-        if ittirate == 1000:
-            break
+        #if ittirate == 1000000:
+            #break
     
 
 
@@ -58,7 +59,7 @@ def cleaning (start:int, start1:int, data_dic:dict): # clean passed  nucleotids
 def peak_start(start1:int, data_dic:dict): #TODO will combine peak_start and end functions
     signal = False
     peak_start = None
-    if data_dic[start1]  == THERESHOLD:
+    if data_dic[start1]  == THERESHOLD:#5
         for i in range(start1, start1+WINDOW-1):# if just one of pos is 5 it would not be peak
             if data_dic[i] != data_dic[start1]:
                 signal = False
@@ -71,16 +72,23 @@ def peak_end(pre_start:int,start1:int, data_dic:dict):
     signal = True
     nucleotid = data_dic[start1]
     nucleotid_pos = start1
-    end = None
+    end = 0
 
     if nucleotid >= THERESHOLD:
         signal = True
     else:
         signal = False
-        for nucleotid in range(pre_start,start1):
-            if data_dic[nucleotid] < THERESHOLD:
-                end = data_dic[nucleotid]
+        for pos in range(pre_start,start1):#must detect from last seq start point
+            try:
+                if data_dic[pos] < THERESHOLD:
+                    end = pos
+            except KeyError:
+                end = pos
 
+
+            
+                    
     return signal, end
 main()
+print("finish")
       
